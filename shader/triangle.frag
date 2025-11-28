65e2353defd1c25882873fd7afe6f2534d666a29
wxc <softsora@163.com>
2025-11-28 12:02:24 +0800

    添加变换的颜色

--- shader/triangle.frag
+++ shader/triangle.frag
@@ -1,7 +1,9 @@
-#version 330 core
+#version 450 core
 layout(location = 0) out vec4 FragColor;
-
+layout(set = 0, binding = 0) uniform InColor {
+    vec4 color;
+} ubo;
 void main()
 {
-    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    FragColor = vec4(ubo.color);
 }
--- src/graphics/app.cpp
+++ src/graphics/app.cpp
@@ -5,6 +5,8 @@
 #include "app.hpp"
 #include "render/gl/render_gl.hpp"
 #include "render/gl/gl_common/shader_util.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <print>
 namespace graphics {
 App::App() : window_(1920, 1080, "OpenGL"), render_base(std::make_unique<render::RenderGL>()) {}
@@ -26,19 +28,19 @@ void App::run() {
         std::println("link shader error {}", infoLog);
     }
 
-
     glDeleteShader(vertex_shader);
     glDeleteShader(fragment_shader);
 
-       // set up vertex data (and buffer(s)) and configure vertex attributes
+    // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices[] = {
-         0.5f,  0.5f, 0.0f,  // top right
-         0.5f, -0.5f, 0.0f,  // bottom right
+        0.5f,  0.5f,  0.0f,  // top right
+        0.5f,  -0.5f, 0.0f,  // bottom right
         -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left
+        -0.5f, 0.5f,  0.0f   // top left
     };
-    unsigned int indices[] = {  // note that we start from 0!
+    unsigned int indices[] = {
+        // note that we start from 0!
         0, 1, 3,  // first Triangle
         1, 2, 3   // second Triangle
     };
@@ -62,12 +64,38 @@ void App::run() {
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
-        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+    GLuint blockIndex = glGetUniformBlockIndex(shaderProgram, "InColor");
+
+    glm::vec4 myColor{1.f};
+
+    GLuint uboHandle{};
+    glGenBuffers(1, &uboHandle);
+    glBindBuffer(GL_UNIFORM_BUFFER, uboHandle);
+    glBufferData(GL_UNIFORM_BUFFER, sizeof(glm::vec4), glm::value_ptr(myColor), GL_DYNAMIC_DRAW);
 
     while (!window_.shouldClose()) {
         render_base->clear(clear_color);
+
+        auto timeValue = static_cast<float>(glfwGetTime());
+        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+        myColor.y = greenValue;
+
+        // 更新 UBO 数据
+        glBindBuffer(GL_UNIFORM_BUFFER, uboHandle);
+        glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(glm::vec4), glm::value_ptr(myColor));
+        glBindBuffer(GL_UNIFORM_BUFFER, 0);
+
+        GLuint bindingPoint = 0;  // 对应 layout(binding = 0)
+        glUniformBlockBinding(shaderProgram, blockIndex, bindingPoint);
+        // 绑定到 binding point
+        glBindBufferBase(GL_UNIFORM_BUFFER, bindingPoint, uboHandle);
+
+
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        glBindVertexArray(VAO);  // seeing as we only have a single VAO there's no need to bind it
+                                 // every time, but we'll do so to keep things a bit more organized
 
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         window_.pullEvent();
